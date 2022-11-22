9031e6eacfe7db6ef0a249a0dceb8476a160a810
Pierfran992 <santoro.pierfrancesco@hotmail.it>
2022-11-22 16:00:51 +0100

    Stato sviluppo: -Inserito nel file myscript.js il nuovo array contenente gli oggetti così da sostituire il precedente;

--- js/myscript.js
+++ js/myscript.js
@@ -1,11 +1,39 @@
-// creo l'array contenente le immagini
-const msImgSlider = ["img/01.webp","img/02.webp","img/03.webp","img/04.webp","img/05.webp",];
+// creo l'array contenente gli oggetti immagini
+const msImgSlider = [ 
+    { 
+        image: 'img/01.webp', 
+        title: 'Marvel\'s Spiderman Miles Morale', 
+        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.' 
+    },
 
+    { 
+        image: 'img/02.webp', 
+        title: 'Ratchet & Clank: Rift Apart', 
+        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.'
+    }, 
+
+    { 
+        image: 'img/03.webp', 
+        title: 'Fortnite', 
+        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos." 
+    }, 
+    
+    { 
+        image: 'img/04.webp', 
+        title: 'Stray', 
+        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city' 
+    }, 
+    
+    { 
+        image: 'img/05.webp', 
+        title: "Marvel's Avengers", 
+        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.' 
+    }];
 console.log(msImgSlider);
 
 const msImgContainer = document.getElementById("imgContainer");
 
-// creo il ciclo for per inserire le immagini nello slider
+/* // creo il ciclo for per inserire le immagini nello slider
 for (let i = 0; i < msImgSlider.length;  i++) {
     // creo il contenitore della singola immagine
     let imgDiv = document.createElement("div");
@@ -15,8 +43,6 @@ for (let i = 0; i < msImgSlider.length;  i++) {
     } else {
         imgDiv.className = "ms_img";
     }
-
-    // imgDiv.className = i == 0? "ms_img ms_active": "ms_img"; IF INLINE POSSO USARLO? 
 
     // creo il tag img
     let img = document.createElement("img");
@@ -28,12 +54,10 @@ for (let i = 0; i < msImgSlider.length;  i++) {
     imgDiv.appendChild(img);
 
     msImgContainer.appendChild(imgDiv);
-
-}
+} */
 
 // creo la costante per richimare le immagini dello slider
 const imgSlider = document.getElementsByClassName("ms_img");
-
 console.log(imgSlider);
 
 // creo la costante che mi permetterà di switchare tra le varie immagini la classe ms_active
@@ -83,3 +107,15 @@ msPrev.addEventListener('click',
     }
 
 )
+
+
+// FUNZIONI
+// Creo la funzione per generare vari elementi con classi o id
+function createElement (typeElement, idClassElement) {
+    // creo l'elemento
+    const element = document.createElement(typeElement);
+    element.classList.add(idClassElement);
+
+    // ritorno l'elemento
+    return element;
+} 
